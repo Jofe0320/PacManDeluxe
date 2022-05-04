399fd3d86b80f992c00a0f881b6aa9730f13f99c
Jofe0320 <jose.ortiz107@upr.edu>
2022-05-04 16:38:06 -0400

    Compare method in PowerUp class

--- src/Game/PowerUps/PowerUp.h
+++ src/Game/PowerUps/PowerUp.h
@@ -6,6 +6,14 @@ class PowerUp{
     PowerUp(){}
     int getRank(){return this->rank;}
     void setRank(int r){this->rank = r;}
+    bool compare(PowerUp* t){
+        if (this->getRank() > t->getRank()){
+            return true;  // return true if target is bigger than t 
+        }else{
+            return false;
+        }
+    }
+    //
     virtual void activate() = 0; //virtual Method
 
 };
